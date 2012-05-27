52b67c9fe3104f04e50f446945e1e0ad1f5e78b5
Alan Barrett <alanbarrett@ireland.com>
2012-05-27 16:24:25 +0100

    Doc

--- course/admin_documentation.php
+++ course/admin_documentation.php
@@ -540,7 +540,7 @@ Note: At a later stage (when assignments are setup), all the assignments should 
 Go to <a href="http://courses.peoples-uni.org/course/applications.php" target="_blank">http://courses.peoples-uni.org/course/applications.php</a>. You can see a list of applicants for this semester.
 </li>
 <li>
-It is possible to filter the applications by semester (if you do not just want the current semester), by approval/registration status, by date, by name or email, by payment status, by whether the student is a Re&#8209;enrolment or a New student, by whether the student has applied for (or is) MMU MPH and by whether the student has applied for a Scholarship. When you have selected your desired filters you should then click "Apply Filters". You may also "Reset Filters" to remove them.
+It is possible to filter the applications by semester (if you do not just want the current semester), by approval/registration status, by date, by name or email, by payment status, by whether the student is a Re&#8209;enrolment or a New student, by whether the student has applied for (or says they are already in) MMU MPH, by whether the student has been accepted into MMU MPH (and, if desired, during which period they were accepted) and by whether the student has applied for a Scholarship. When you have selected your desired filters you should then click "Apply Filters". You may also "Reset Filters" to remove them.
 </li>
 <li>
 It is also possible to display only the columns relevant to Scholarships by clicking "Show Scholarship Relevant Columns" and then "Apply Filters".
