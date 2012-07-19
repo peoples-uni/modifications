7e4967668b6ed4d7c6e9441cb29121cbc46e36d5
Alan Barrett <alanbarrett@ireland.com>
2012-07-19 07:46:54 +0100

    Student Account

--- course/account.php
+++ course/account.php
@@ -43,7 +43,7 @@ $mphs = $DB->get_records_sql("SELECT * FROM mdl_peoplesmph WHERE (sid=$sid AND s
 if (!empty($mphs)) {
   foreach ($mphs as $mph) {
     $inmmumph = TRUE;
-    echo '<br />Student was Enrolled in MMU MPH on ' . gmdate('d/m/Y H:i', $mph->datesubmitted) . '<br />';
+    echo '<br />You were Enrolled in MMU MPH on ' . gmdate('d/m/Y H:i', $mph->datesubmitted) . '<br />';
   }
 }
 
@@ -72,66 +72,9 @@ if (!empty($balances)) {
   echo html_writer::table($table);
 }
 else {
-    echo '<br />No Transactions for this Student';
+  echo '<br />No Transactions';
 }
 
-?>
-<br /><br />
-<p>To adjust the student balance, enter the Payment Amount & the Detail and then click "Subtract a new Payment Amount from the Student Balance".<br />
-(The should will be +ve if th student has made a payment or are given a full/part Bursary)<br />
-(Prefix the Amount with a "-" if it should increase the balance owed by the Student.)</p>
-
-<form id="setowedform" method="post" action="<?php echo $CFG->wwwroot . '/course/payconfirm.php'; ?>">
-
-<input type="hidden" name="sid" value="<?php echo $sid; ?>" />
-<input type="hidden" name="sesskey" value="<?php echo $USER->sesskey ?>" />
-Payment Amount: <input type="text" size="60" name="amount_delta" value="" /><br />
-Detail: <input type="text" size="60" name="detail" value="" /><br />
-<br />
-
-<input type="hidden" name="marksetowed" value="1" />
-<input type="submit" name="setowed" value="Subtract a new Payment Amount from the Student Balance" />
-</form>
-
-
-<br /><br /><br /><p>Update the payment method/status and then click "Submit the Payment Method/Status".<br />
-(If you set a "Confirmed" status or "Send e-mail...", the student will be e-mailed with their transactions and current Balance,<br />
-so be sure to set the correct balance first.)</p>
-
-<form id="payconfirmform" method="post" action="<?php echo $CFG->wwwroot . '/course/payconfirm.php'; ?>">
-
-<input type="hidden" name="sid" value="<?php echo $sid; ?>" />
-<input type="hidden" name="sesskey" value="<?php echo $USER->sesskey ?>" />
-
-Select the new payment method/status: <select name="paymentmechanism">
-<option value="0">Select one...</option>
-<option value="2">Barclays Bank Transfer</option>
-<option value="10">Ecobank Nigeria PLC. Transfer</option>
-<option value="3">Diamond Bank Plc. Transfer</option>
-<option value="9">MoneyGram Payment</option>
-<option value="4">Western Union Payment</option>
-<option value="5">Indian Confederation for Healthcare Accreditation for those taking Patient Safety module from India</option>
-<option value="7">Posted Travellers Cheques</option>
-<option value="8">Posted Cash</option>
-<option value="6">Promised to Pay by End of Semester</option>
-<option value="100">Waiver</option>
-<option value="102">Confirmed: Barclays Bank Transfer</option>
-<option value="110">Confirmed: Ecobank Transfer</option>
-<option value="103">Confirmed: Diamond Bank Transfer</option>
-<option value="109">Confirmed: MoneyGram</option>
-<option value="104">Confirmed: Western Union</option>
-<option value="105">Confirmed: Indian Confederation</option>
-<option value="107">Confirmed: Posted Travellers Cheques</option>
-<option value="108">Confirmed: Posted Cash</option>
-<option value="199">Send e-mail without changing Status</option>
-</select><br /><br />
-
-<input type="hidden" name="markpayconfirm" value="1" />
-<input type="submit" name="payconfirm" value="Submit the Payment Method/Status" />
-</form>
-
-
-<?php
 $payment_schedule = $DB->get_record('peoples_payment_schedule', array('userid' => $userid));
 if (!empty($payment_schedule)) {
   echo '<br /><br /><br />';
@@ -160,165 +103,11 @@ if (!empty($payment_schedule)) {
 
   $user_who_modified = $DB->get_record('user', array('id' => $payment_schedule->user_who_modified));
   echo '<br />(last modified by ' . fullname($user_who_modified) . ' on ' . gmdate('d/m/Y H:i', $payment_schedule->date_modified) . ')';
-
-  echo '<br /><a href="' . $CFG->wwwroot . '/course/specify_instalments.php?userid=' . $userid . '" target="_blank">Change Instalments for this Student</a>';
-}
-else {
-  echo '<br /><br /><a href="' . $CFG->wwwroot . '/course/specify_instalments.php?userid=' . $userid . '" target="_blank">Specify Instalments for this Student (normally done by Student themselves)</a>';
 }
 
-echo '<br /><br /><br />';
-$paymentnotes = $DB->get_records_sql("SELECT * FROM mdl_peoplespaymentnote WHERE (sid=$sid AND sid!=0) OR (userid={$application->userid} AND userid!=0) ORDER BY datesubmitted DESC");
-if (!empty($paymentnotes)) {
-  echo "<table border=\"1\" BORDERCOLOR=\"RED\">";
-  echo '<tr><td colspan="2">Payment Notes (can add more below)...</td></tr>';
-
-  foreach ($paymentnotes as $paymentnote) {
-    echo '<tr><td>';
-    echo gmdate('d/m/Y H:i', $paymentnote->datesubmitted);
-    echo '</td><td>';
-    echo $paymentnote->note;
-    echo '</td></tr>';
-  }
-
-  echo '</table>';
-}
-?>
-
-<form id="paymentnoteform" method="post" action="<?php echo $CFG->wwwroot . '/course/payconfirm.php'; ?>">
-
-<input type="hidden" name="sid" value="<?php echo $sid; ?>" />
-<input type="hidden" name="sesskey" value="<?php echo $USER->sesskey ?>" />
-<textarea name="note" rows="5" cols="100" wrap="hard"></textarea><br />
-
-<input type="hidden" name="markpaymentnote" value="1" />
-<input type="submit" name="paymentnote" value="Add this Payment Note to this Student (will be seen on this page in future semesters)" />
-</form>
-
-
-<?php
-$applications = $DB->get_records_sql("
-  SELECT DISTINCT a.sid AS appsid, a.*
-  FROM mdl_peoplesapplication a
-  WHERE a.userid={$application->userid} AND a.userid!=0 AND sid!=$sid AND hidden=0
-  ORDER BY a.datesubmitted DESC");
-if (!empty($applications)) {
-
-  echo '<br /><br /><br />Other Applications...<br />';
-
-  $table = new html_table();
-  $table->head = array(
-    'Semester',
-    'sid',
-    'Payment Status',
-    'Registered?',
-  );
-
-  foreach ($applications as $sid => $app) {
-    $state = (int)$app->state;
-    if ($state === 2) {
-      $state = 022;
-    }
-    if ($state === 1) {
-      $state = 011;
-    }
-    $state1 = $state & 07;
-    $state2 = $state & 070;
-
-    $rowdata = array();
-    $rowdata[] = htmlspecialchars($app->semester, ENT_COMPAT, 'UTF-8');
-    $rowdata[] = $sid;
-
-    if (empty($app->paymentmechanism)) $mechanism = '';
-    elseif ($app->paymentmechanism == 1) $mechanism = 'RBS Confirmed';
-    elseif ($app->paymentmechanism == 2) $mechanism = 'Barclays';
-    elseif ($app->paymentmechanism == 3) $mechanism = 'Diamond';
-    elseif ($app->paymentmechanism ==10) $mechanism = 'Ecobank';
-    elseif ($app->paymentmechanism == 4) $mechanism = 'Western Union';
-    elseif ($app->paymentmechanism == 5) $mechanism = 'Indian Confederation';
-    elseif ($app->paymentmechanism == 6) $mechanism = 'Promised End Semester';
-    elseif ($app->paymentmechanism == 7) $mechanism = 'Posted Travellers Cheques';
-    elseif ($app->paymentmechanism == 8) $mechanism = 'Posted Cash';
-    elseif ($app->paymentmechanism == 9) $mechanism = 'MoneyGram';
-    elseif ($app->paymentmechanism == 100) $mechanism = 'Waiver';
-    elseif ($app->paymentmechanism == 102) $mechanism = 'Barclays Confirmed';
-    elseif ($app->paymentmechanism == 103) $mechanism = 'Diamond Confirmed';
-    elseif ($app->paymentmechanism == 110) $mechanism = 'Ecobank Confirmed';
-    elseif ($app->paymentmechanism == 104) $mechanism = 'Western Union Confirmed';
-    elseif ($app->paymentmechanism == 105) $mechanism = 'Indian Confederation Confirmed';
-    elseif ($app->paymentmechanism == 107) $mechanism = 'Posted Travellers Cheques Confirmed';
-    elseif ($app->paymentmechanism == 108) $mechanism = 'Posted Cash Confirmed';
-    elseif ($app->paymentmechanism == 109) $mechanism = 'MoneyGram Confirmed';
-    else  $mechanism = '';
-    //if ($app->costpaid < .01) $z = '<span style="color:red">No' . $mechanism . '</span>';
-    //elseif (abs($app->costowed - $app->costpaid) < .01) $z = '<span style="color:green">Yes' . $mechanism . '</span>';
-    //else $z = '<span style="color:blue">' . "Paid $app->costpaid out of $app->costowed" . $mechanism . '</span>';
-    //$rowdata[] = $z;
-    $rowdata[] = $mechanism;
-
-    if (!($state1===03 || $state2===030)) $z = '<span style="color:red">No</span>';
-    elseif ($state === 033) $z = '<span style="color:green">Yes</span>';
-    else $z = '<span style="color:blue">Some</span>';
-    $rowdata[] = $z;
-
-    //$rowdata[] = $app->costowed;
-    //$rowdata[] = $app->costpaid;
-
-    $table->data[] = $rowdata;
-  }
-  echo html_writer::table($table);
-}
-?>
-
-
-<br /><br />
-</div>
-
-<?php
+echo '<br /><br />';
+echo '</div>';
 
 echo $OUTPUT->box_end();
 echo $OUTPUT->footer();
-
-
-function sendapprovedmail($email, $subject, $message) {
-  global $CFG;
-
-  // Dummy User
-  $user = new stdClass();
-  $user->id = 999999999;
-  $user->email = $email;
-  $user->maildisplay = true;
-  $user->mnethostid = $CFG->mnet_localhost_id;
-
-  $supportuser = new stdClass();
-  $supportuser->email = 'payments@peoples-uni.org';
-  $supportuser->firstname = 'Peoples-uni Payments';
-  $supportuser->lastname = '';
-  $supportuser->maildisplay = true;
-
-  //$user->email = 'alanabarrett0@gmail.com';
-  $ret = email_to_user($user, $supportuser, $subject, $message);
-
-  $user->email = 'applicationresponses@peoples-uni.org';
-
-  //$user->email = 'alanabarrett0@gmail.com';
-  email_to_user($user, $supportuser, $email . ' Sent: ' . $subject, $message);
-
-  return $ret;
-}
-
-
-function get_balance($userid) {
-  global $DB;
-
-  $balances = $DB->get_records_sql("SELECT * FROM mdl_peoples_student_balance WHERE userid={$userid} ORDER BY id DESC LIMIT 1");
-  $amount = 0;
-  if (!empty($balances)) {
-    foreach ($balances as $balance) {
-      $amount = $balance->balance;
-    }
-  }
-
-  return $amount;
-}
 ?>
