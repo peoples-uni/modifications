22d5244a2e96c4937952d60e861c84c82a32aca4
Alan Barrett <alanbarrett@ireland.com>
2012-07-20 17:40:40 +0100

    Doc account.php

--- course/admin_documentation.php
+++ course/admin_documentation.php
@@ -1102,86 +1102,24 @@ You can also list all certificates with the "List All Certificates" link.
 <h2>Page to allow a Student to see their Payment Account</h2>
 <ol>
 <li>
+<a href="http://courses.peoples-uni.org/course/account.php">http://courses.peoples-uni.org/course/account.php</a> allows a student to see their account, it is no direct use to staff (see "Update Payment Amounts or Method/Status"/payconfirm.php in <a href="http://courses.peoples-uni.org/course/admin_documentation.php#thedetailsbutton">The "Details" button and the Approval Process</a> for a staff view on these data).
 </li>
-[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
-(**)+++ b/course/account.php
-
-+echo '<br /><br />' . fullname($userrecord) . '<br />';
-+
-+$inmmumph = FALSE;
-+$mphs = $DB->get_records_sql("SELECT * FROM mdl_peoplesmph WHERE (sid=$sid AND sid!=0) OR (userid={$application->userid} AND userid!=0) ORDER BY datesubmitted DESC LIMIT 1");
-+if (!empty($mphs)) {
-+  foreach ($mphs as $mph) {
-+    $inmmumph = TRUE;
-+    echo '<br />You were Enrolled in MMU MPH on ' . gmdate('d/m/Y H:i', $mph->datesubmitted) . '<br />';
-+  }
-+}
-+
-+echo "</b></p>";
-+
-+$balances = $DB->get_records_sql("SELECT * FROM mdl_peoples_student_balance WHERE userid={$userid} ORDER BY id");
-+if (!empty($balances)) {
-+  $table = new html_table();
-+
-+  $table->head = array(
-+    'Date',
-+    'Detail',
-+    'Transaction Amount &pound;s',
-+    'Balance &pound;s (+ve means the Student Owes us)',
-+  );
-+
-+  foreach ($balances as $balance) {
-+    $rowdata = array();
-+    $rowdata[] = gmdate('d/m/Y H:i', $balance->date);
-+    $rowdata[] = htmlspecialchars($balance->detail, ENT_COMPAT, 'UTF-8');
-+    $rowdata[] = number_format($balance->amount_delta, 2);
-+    $rowdata[] = number_format($balance->balance, 2);
-+    $table->data[] = $rowdata;
-+  }
-
-+else {
-+  echo '<br />No Transactions';
-+}
-+
-+$payment_schedule = $DB->get_record('peoples_payment_schedule', array('userid' => $userid));
-+if (!empty($payment_schedule)) {
-+  echo '<br /><br /><br />';
-+  $table = new html_table();
-+  $table->head = array(
-+    'Last Date for Payment of Instalment 1',
-+    'Instalment 1 Amount',
-+    'Last Date for Payment of Instalment 2',
-+    'Instalment 2 Amount',
-+    'Last Date for Payment of Instalment 3',
-+    'Instalment 3 Amount',
-+    'Last Date for Payment of Instalment 4',
-+    'Instalment 4 Amount',
-+  );
-+  $rowdata = array();
-+  $rowdata[] = gmdate('d/m/Y', $payment_schedule->due_date_1);
-+  $rowdata[] = '&pound;' . number_format($payment_schedule->amount_1, 2);
-+  $rowdata[] = gmdate('d/m/Y', $payment_schedule->due_date_2);
-+  $rowdata[] = '&pound;' . number_format($payment_schedule->amount_2, 2);
-+  $rowdata[] = gmdate('d/m/Y', $payment_schedule->due_date_3);
-+  $rowdata[] = '&pound;' . number_format($payment_schedule->amount_3, 2);
-+  $rowdata[] = gmdate('d/m/Y', $payment_schedule->due_date_4);
-+  $rowdata[] = '&pound;' . number_format($payment_schedule->amount_4, 2);
-+  $table->data[] = $rowdata;
-+  echo html_writer::table($table);
-+
-+  $user_who_modified = $DB->get_record('user', array('id' => $payment_schedule->user_who_modified));
-+  echo '<br />(last modified by ' . fullname($user_who_modified) . ' on ' . gmdate('d/m/Y H:i', $payment_schedule->date_modified) . ')';
-]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+<li>
+It shows:-
+<br />Whether they are enrolled in MMU MPH.
+<br />All their payment account transactions.
+<br />Their instalment payment schedule, if any.
+</li>
 </ol>
 
 
 <a name="specifyinstalments"></a>
 <h2>Specify Instalment Payment Schedule</h2>
 <ol>
-<li>
-</li>
 [[[[[[[[[[[[[[[[[[[[[[
 (**)+++ b/course/specify_instalments.php
+<li>
+</li>
 +require_login();
 +if (empty($USER->id)) {echo '<h1>Not properly logged in, should not happen!</h1>'; die();}
 +$ismanager = has_capability('moodle/site:viewparticipants', get_context_instance(CONTEXT_SYSTEM));
