2891e2bf6038b66480dba118740950d901b29aeb
Alan Barrett <alanbarrett@ireland.com>
2012-07-21 16:45:45 +0100

    Specify instalments

--- course/admin_documentation.php
+++ course/admin_documentation.php
@@ -1116,151 +1116,24 @@ It shows:-
 <a name="specifyinstalments"></a>
 <h2>Specify Instalment Payment Schedule</h2>
 <ol>
-[[[[[[[[[[[[[[[[[[[[[[
-(**)+++ b/course/specify_instalments.php
 <li>
+This is used for the student to specify an instalment payment schedule for themselves. It can also be used (via a link in payconfirm.php) by staff to specify or change an instalment payment schedule. The student is only allowed do this once, after that it can only be changed by staff. The student should be/will be notified of the page to use for this in their approval e-mail.
 </li>
-+require_login();
-+if (empty($USER->id)) {echo '<h1>Not properly logged in, should not happen!</h1>'; die();}
-+$ismanager = has_capability('moodle/site:viewparticipants', get_context_instance(CONTEXT_SYSTEM));
-+$userid = optional_param('userid', 0, PARAM_INT);
-+if (empty($userid) || !$ismanager) $userid = $USER->id;
-
-+if (!empty($_POST['markspecifyinstalments'])) {
-+  if (!isset($_POST['amount_1']) || !is_numeric($_POST['amount_1'])) notice('Instalment 1 must be a number. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if (!isset($_POST['amount_2']) || !is_numeric($_POST['amount_2'])) notice('Instalment 2 must be a number. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if (!isset($_POST['amount_3']) || !is_numeric($_POST['amount_3'])) notice('Instalment 3 must be a number. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if (!isset($_POST['amount_4']) || !is_numeric($_POST['amount_4'])) notice('Instalment 4 must be a number. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if ($_POST['amount_4'] != 0.0 && $_POST['amount_3'] == 0.0) notice('Instalment 3 must be non-zero if Instalment 4 is non-zero. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if ($_POST['amount_4'] != 0.0 && $_POST['amount_2'] == 0.0) notice('Instalment 2 must be non-zero if Instalment 4 is non-zero. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if ($_POST['amount_3'] != 0.0 && $_POST['amount_2'] == 0.0) notice('Instalment 2 must be non-zero if Instalment 3 is non-zero. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if (                             ($_POST['amount_1']/$amount_to_pay_total) <= 0.24) notice('Instalment 1 must be at least 25% of the amount owed (UK Pounds ' . $amount_to_pay_total . '). Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if ($_POST['amount_2'] != 0.0 && ($_POST['amount_2']/$amount_to_pay_total) <= 0.24) notice('Instalment 2 must be at least 25% of the amount owed (UK Pounds ' . $amount_to_pay_total . '), or it can be zero if there is only 1 instalment. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if ($_POST['amount_3'] != 0.0 && ($_POST['amount_3']/$amount_to_pay_total) <= 0.24) notice('Instalment 3 must be at least 25% of the amount owed (UK Pounds ' . $amount_to_pay_total . '), or it can be zero if there are only 2 instalments. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if ($_POST['amount_4'] != 0.0 && ($_POST['amount_4']/$amount_to_pay_total) <= 0.24) notice('Instalment 4 must be at least 25% of the amount owed (UK Pounds ' . $amount_to_pay_total . '), or it can be zero. Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+  if (abs($_POST['amount_2'] + $_POST['amount_2'] + $_POST['amount_3'] + $_POST['amount_4'] - $amount_to_pay_total) > .01) notice('Total must add up to amount owed (UK Pounds ' . $amount_to_pay_total . '). Press Continue and re-specify amounts.', "$CFG->wwwroot/course/specify_instalments.php?userid=$userid");
-+
-+  $peoples_payment_schedule = $DB->get_record('peoples_payment_schedule', array('userid' => $userid));
-+  if (empty($peoples_payment_schedule)) {
-+    $peoples_payment_schedule = new object();
-+    $insert = TRUE;
-+    $peoples_payment_schedule->userid = $userid;
-+    $peoples_payment_schedule->currency = 'GBP';
-+  }
-+  else {
-+    $insert = FALSE;
-+  }
-+  $peoples_payment_schedule->amount_1 = $_POST['amount_1'];
-+  $peoples_payment_schedule->amount_2 = $_POST['amount_2'];
-+  $peoples_payment_schedule->amount_3 = $_POST['amount_3'];
-+  $peoples_payment_schedule->amount_4 = $_POST['amount_4'];
-+
-+  $year = (int)gmdate('Y');
-+  $month = (int)gmdate('n');
-+  if ($month <= 6) {
-+    $peoples_payment_schedule->expect_amount_1_date = gmmktime(0, 0, 0,  1, 1, $year);
-+    $peoples_payment_schedule->due_date_1           = gmmktime(0, 0, 0,  4, 1, $year);
-+    $peoples_payment_schedule->expect_amount_2_date = gmmktime(0, 0, 0,  7, 1, $year);
-+    $peoples_payment_schedule->due_date_2           = gmmktime(0, 0, 0, 10, 1, $year);
-+    $peoples_payment_schedule->expect_amount_3_date = gmmktime(0, 0, 0,  1, 1, $year + 1);
-+    $peoples_payment_schedule->due_date_3           = gmmktime(0, 0, 0,  4, 1, $year + 1);
-+    $peoples_payment_schedule->expect_amount_4_date = gmmktime(0, 0, 0,  7, 1, $year + 1);
-+    $peoples_payment_schedule->due_date_4           = gmmktime(0, 0, 0, 10, 1, $year + 1);
-+  }
-+  else {
-+    $peoples_payment_schedule->expect_amount_1_date = gmmktime(0, 0, 0,  7, 1, $year);
-+    $peoples_payment_schedule->due_date_1           = gmmktime(0, 0, 0, 10, 1, $year);
-+    $peoples_payment_schedule->expect_amount_2_date = gmmktime(0, 0, 0,  1, 1, $year + 1);
-+    $peoples_payment_schedule->due_date_2           = gmmktime(0, 0, 0,  4, 1, $year + 1);
-+    $peoples_payment_schedule->expect_amount_3_date = gmmktime(0, 0, 0,  7, 1, $year + 1);
-+    $peoples_payment_schedule->due_date_3           = gmmktime(0, 0, 0, 10, 1, $year + 1);
-+    $peoples_payment_schedule->expect_amount_4_date = gmmktime(0, 0, 0,  1, 1, $year + 2);
-+    $peoples_payment_schedule->due_date_4           = gmmktime(0, 0, 0,  4, 1, $year + 2);
-+  }
-+
-+  $peoples_payment_schedule->user_who_modified = $USER->id;
-+  $peoples_payment_schedule->date_modified = time();
-+  if ($insert) {
-+    $DB->insert_record('peoples_payment_schedule', $peoples_payment_schedule);
-+  }
-+  else {
-+    $DB->update_record('peoples_payment_schedule', $peoples_payment_schedule);
-+  }
-+}
-
-+echo '<br /><br />' . fullname($userrecord);
-+
-+$inmmumph = FALSE;
-+$mphs = $DB->get_records_sql("SELECT * FROM mdl_peoplesmph WHERE userid={$userid} AND userid!=0 LIMIT 1");
-+if (!empty($mphs)) {
-+  foreach ($mphs as $mph) {
-+    $inmmumph = TRUE;
-+  }
-+}
-+
-+$payment_schedule = $DB->get_record('peoples_payment_schedule', array('userid' => $userid));
-+if (!empty($payment_schedule)) {
-+  echo '<br /><br /><br />';
-+  $table = new html_table();
-+  $table->head = array(
-+    'Last Date for Payment of Instalment 1',
-+    'Instalment 1 Amount',
-+    'Last Date for Payment of Instalment 2',
-+    'Instalment 2 Amount',
-+    'Last Date for Payment of Instalment 3',
-+    'Instalment 3 Amount',
-+    'Last Date for Payment of Instalment 4',
-+    'Instalment 4 Amount',
-+  );
-+  $rowdata = array();
-+  $rowdata[] = gmdate('d/m/Y', $payment_schedule->due_date_1);
-+  $rowdata[] = '&pound;' . number_format($payment_schedule->amount_1, 2);
-+  $rowdata[] = gmdate('d/m/Y', $payment_schedule->due_date_2);
-+  $rowdata[] = '&pound;' . number_format($payment_schedule->amount_2, 2);
-+  $rowdata[] = gmdate('d/m/Y', $payment_schedule->due_date_3);
-+  $rowdata[] = '&pound;' . number_format($payment_schedule->amount_3, 2);
-+  $rowdata[] = gmdate('d/m/Y', $payment_schedule->due_date_4);
-+  $rowdata[] = '&pound;' . number_format($payment_schedule->amount_4, 2);
-+  $table->data[] = $rowdata;
-+  echo html_writer::table($table);
-+
-+  $user_who_modified = $DB->get_record('user', array('id' => $payment_schedule->user_who_modified));
-+  echo '<br />(last modified by ' . fullname($user_who_modified) . ' on ' . gmdate('d/m/Y H:i', $payment_schedule->date_modified) . ')';
-+}
-+
-+echo '</b></p>';
-+
-+if ($inmmumph && (empty($payment_schedule) || $ismanager)) {
-+?>
-+<br /><br /><br /><p>Specify the Payment Schedule and then click "Submit the Instalment Payment Schedule".<br />
-+You can enter up to four instalments.<br />
-+Any individual (non zero) instalment must be at least 25% of what is owed (UK Pounds <?php echo $amount_to_pay_total ?>).<br />
-+
-+<?php
-+  if (!$ismanager) {
-+    echo '<b>Note: You can only enter this schedule once!</b><br />';
-+    echo '<b>Note: All payments must be completed before you start your Masters dissertation,</b><br />';
-+    echo '<b>please choose instalments accordingly! If this is a problem please e-mail <a href="mailto:payments@peoples-uni.org?subject=Instalment query">payments@peoples-uni.org</a></b><br />';
-+  }
-+
-+Instalment (UK Pounds) due on or before <?php gmdate('d/m/Y', $peoples_payment_schedule->due_date_1); ?>: <input type="text" size="10" name="amount_1" value="<?php echo $amount_to_pay_total ?>" /><br />
-+Instalment (UK Pounds) due on or before <?php gmdate('d/m/Y', $peoples_payment_schedule->due_date_2); ?>: <input type="text" size="10" name="amount_2" value="0" /><br />
-+Instalment (UK Pounds) due on or before <?php gmdate('d/m/Y', $peoples_payment_schedule->due_date_3); ?>: <input type="text" size="10" name="amount_3" value="0" /><br />
-+Instalment (UK Pounds) due on or before <?php gmdate('d/m/Y', $peoples_payment_schedule->due_date_4); ?>: <input type="text" size="10" name="amount_4" value="0" /><br />
-
-+  if ($ismanager) {
-+    echo '<b>This Student is not in the MMU MPH!</b><br />';
-+  }
-+  else {
-+    if (!$inmmumph) {
-+      echo '<b>You are not in the MMU MPH!</b><br />';
-+    }
-+    else {
-+      echo '<b>You have already set your payment schedule. If there is a need to change it please e-mail <a href="mailto:payments@peoples-uni.org?subject=Instalment query">payments@peoples-uni.org</a></b><br />';
-+    }
-+  }
-+}
-]]]]]]]]]]]]]]]]]]]]]]
+<li>
+There can be any number up to four instalments starting from the current semester. There can be no gaps (a semester with a zero instalment between non-zero instalments). Any instalment must be at least 25% of the total owed.
+</li>
+<li>
+The total owed will be taken from student's payment account balance, so that will need to be correct.
+</li>
+<li>
+The page indicates that the deadline for payment is either 1st of April or October, although we do not enforce that (we continue to take payments after that.).
+</li>
+<li>
+Elsewhere in the software when payment is being asked for, the amount being asked for will be increased to include a new instalment for the upcoming semester whenever January 1st or July 1st is reached. So if a student is very late in paying the instalment due on, say Oct 1st, and they go to pay in the following January, they will be asked for the instalment due on October 1st AND any instalment due on April 1st following.
+</li>
+<li>
+It will be possible to see on the page who has specified the instalment schedule (student or staff member).
+</li>
 </ol>
 
 
