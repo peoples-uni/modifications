e60488db696734923ee748600d0862cda9e5f449
Alan Barrett <alanbarrett@ireland.com>
2012-08-27 16:56:46 +0100

    Should be negative

--- course/payconfirm.php
+++ course/payconfirm.php
@@ -175,7 +175,7 @@ elseif (!empty($_POST['marksetowed'])) {
     notice('Detail must be specified. Press Continue and re-enter.', "$CFG->wwwroot/course/payconfirm.php?sid=$sid");
   }
 
-  $same_payment_should_not_exist = $DB->get_records_sql("SELECT * FROM mdl_peoples_student_balance WHERE userid=? AND amount_delta=?", array($application->userid, $_POST['amount_delta']));
+  $same_payment_should_not_exist = $DB->get_records_sql("SELECT * FROM mdl_peoples_student_balance WHERE userid=? AND amount_delta=?", array($application->userid, -$_POST['amount_delta']));
   if (!empty($same_payment_should_not_exist)) {
     foreach ($same_payment_should_not_exist as $payment) {
       if ($payment->detail == $_POST['detail']) notice('The same Payment Amount and Detail have already been specified for this student. Specify a different Detail. Press Continue and re-enter.', "$CFG->wwwroot/course/payconfirm.php?sid=$sid");
