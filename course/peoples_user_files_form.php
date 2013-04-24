27641a11feb462140a2483de68f717b4a88e7c45
Alan Barrett <alanabarrett0@gmail.com>
2013-04-24 16:17:32 +0100

    add global $DB;

--- course/peoples_user_files_form.php
+++ course/peoples_user_files_form.php
@@ -11,6 +11,8 @@ require_once("$CFG->libdir/formslib.php");
 
 class peoples_user_files_form extends moodleform {
   function definition() {
+    global $DB;
+
     $mform = $this->_form;
 
     $data    = $this->_customdata['data'];
