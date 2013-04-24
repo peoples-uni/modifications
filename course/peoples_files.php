f1ca3fc6298895283de370a4761fc5fe894770b8
Alan Barrett <alanabarrett0@gmail.com>
2013-04-24 16:26:49 +0100

    Add handling for submitted form

--- course/peoples_files.php
+++ course/peoples_files.php
@@ -46,10 +46,10 @@ $data = new stdClass();
 $data->returnurl = $returnurl;
 
 if ($is_manager) {
-  $options = array('subdirs' => 1, 'maxbytes' => 0, 'maxfiles' => -1, 'accepted_types' => '*', 'areamaxbytes' => -1, 'is_manager' => TRUE);
+  $options = array('subdirs' => 1, 'maxbytes' => 0, 'maxfiles' => -1, 'accepted_types' => '*', 'areamaxbytes' => -1, 'is_manager' => TRUE, 'student_id' => $student_id);
 }
 else {
-  $options = array('subdirs' => 1, 'maxbytes' => 1, 'maxfiles' => 0, 'accepted_types' => '', 'areamaxbytes' => 0, 'is_manager' => FALSE);
+  $options = array('subdirs' => 1, 'maxbytes' => 1, 'maxfiles' => 0, 'accepted_types' => '', 'areamaxbytes' => 0, 'is_manager' => FALSE, 'student_id' => $student_id);
 }
 
 $student = $DB->get_record('user', array('id' => $student_id));
@@ -75,7 +75,54 @@ if ($mform->is_cancelled()) {
 }
 elseif ($formdata = $mform->get_data()) {
   $formdata = file_postupdate_standard_filemanager($formdata, 'files', $options, $context, 'peoples_record', 'student', 0);
+
+  if ($is_manager) {
+    $sql = "
+      SELECT CONCAT(filepath, filename, ' FILESIZE:' filesize, ' TIMEMODIFIED:', timemodified) AS file_hash, CONCAT(filepath, filename) AS file_name
+      FROM {files}
+      WHERE contextid=:contextid AND component=:component AND filearea=:filearea AND filesize!=0
+      ORDER BY CONCAT(filepath, filename)";
+    $conditions = array('contextid' => $context->id, 'component' => 'peoples_record', 'filearea' => 'student');
+    $filerecords = $DB->get_records_sql($sql, $conditions);
+    if (!empty($filerecords)) {
+      foreach ($filerecords as $file_hash => $filerecord) {
+        if (!empty($_SESSION['peoples_files_snapshot'][$file_hash])) unset($filerecords[$file_hash]);
+      }
+    }
+
+    if (!empty($filerecords) && empty($formdata->dont_send_email) && !empty($formdata->emailtosend)) {
+      $list_of_files_updated = '';
+      foreach ($filerecords as $filerecord) {
+        $list_of_files_updated .= "$filerecord->file_name\n";
+      }
+
+      $message = str_replace('LIST_OF_FILES', $list_of_files_updated, $formdata->emailtosend);
+
+      $supportuser = new stdClass();
+      $supportuser->email = 'techsupport@peopes-uni.org';
+      $supportuser->firstname = "People's Open Access Education Initiative: Peoples-uni";
+      $supportuser->lastname = '';
+      $supportuser->maildisplay = true;
+
+      email_to_user($student, $supportuser, 'Peoples-uni Record Files Updated', $message);
+    }
+  }
+
   redirect($returnurl);
+}
+else {
+  // The form has not been submitted, it is being displayed
+  if ($is_manager) {
+    $sql = "
+      SELECT CONCAT(filepath, filename, ' FILESIZE:' filesize, ' TIMEMODIFIED:', timemodified) AS file_hash, CONCAT(filepath, filename) AS file_name
+      FROM {files}
+      WHERE contextid=:contextid AND component=:component AND filearea=:filearea AND filesize!=0
+      ORDER BY CONCAT(filepath, filename)";
+    $conditions = array('contextid' => $context->id, 'component' => 'peoples_record', 'filearea' => 'student');
+    $filerecords = $DB->get_records_sql($sql, $conditions);
+    if (!empty($filerecords)) $_SESSION['peoples_files_snapshot'] = $filerecords;
+    else $_SESSION['peoples_files_snapshot'] = array();
+  }
 }
 
 echo $OUTPUT->header();
@@ -84,6 +131,7 @@ echo $OUTPUT->box_start('generalbox');
 echo "<br /><h2>Peoples-uni Record Files for $student->lastname, $student->firstname ($student->id)</h2><br />";
 if (!$is_manager) echo "(Do not 'Add...' a file, 'Create folder' or 'Delete...' etc. as you will not be able to permanently save changes into Moodle.)<br />";
 
+//?????????????????????????$mform->set_data($toform);
 $mform->display();
 
 echo $OUTPUT->box_end();
