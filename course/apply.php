0904358c80f289a73459765dbff109e7ac619cd9
Alan Barrett <alanabarrett0@gmail.com>
2013-04-27 17:05:30 +0100

    If Student clicks "Guest" by mistake it is better to force them to login below (see "if (isguestuser())"), so DO allow override of $SESSION->wantsurl

--- course/apply.php
+++ course/apply.php
@@ -14,8 +14,9 @@ $PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
 $PAGE->set_url('/course/apply.php'); // Defined here to avoid notices on errors etc
 $PAGE->set_pagelayout('standard'); // Standard layout with blocks, this is recommended for most pages with general information
 
-$SESSION->wantsurl = "$CFG->wwwroot/course/application_form_returning_student.php"; // Redirect to here after login
-require_login(NULL, FALSE, NULL, FALSE);  // No course, Don't make user a Guest, No course, Don't override $SESSION->wantsurl
+$SESSION->wantsurl = "$CFG->wwwroot/course/application_form_returning_student.php"; // Redirect to here after login [will not be used see below]
+//require_login(NULL, FALSE, NULL, FALSE);  // No course, Don't make user a Guest, No course, Don't override $SESSION->wantsurl
+require_login(NULL, FALSE);  // No course, Don't make user a Guest [actually if they click "Guest" by mistake it is better to force them to login below (see "if (isguestuser())"), so DO allow override of $SESSION->wantsurl]
 if (empty($USER->id)) {echo '<h1>Not properly logged in, should not happen!</h1>'; die();}
 
 if (isguestuser()) {  // In case user has specifically logged in as Guest (or has been logged in automatically as Guest on some other page)
