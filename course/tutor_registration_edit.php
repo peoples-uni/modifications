8877bc0d613777f6106542f01982f8c2335b6058
Alan Barrett <alanabarrett0@gmail.com>
2013-12-15 18:21:50 +0000

    WIP

---
+++ course/tutor_registration_edit.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Tutor Registration Edit form for Peoples-uni Tutors
+ */
+
+require_once('../config.php');
+require_once('tutor_registration_edit_form.php');
+
+$PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
+
+$PAGE->set_pagelayout('standard');
+$PAGE->set_url('/course/tutor_registration_edit.php');
+
+
+require_login();
+
+// Access is given by the "Manager" role which has moodle/site:viewparticipants
+// (administrator also has moodle/site:viewparticipants)
+//require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM));
+require_capability('moodle/site:viewparticipants', get_context_instance(CONTEXT_SYSTEM));
+
+
+$userid = optional_param('id', 0, PARAM_INT);
+if (empty($userid)) {echo '<h1>$userid empty(), should not happen!</h1>'; die();}
+
+$userrecord = $DB->get_record('user', array('id' => $userid));(**)verify is a tutor??? YES CHECK THER IS A RECORD... OR NO???
+if (empty($userrecord)) {
+  echo '<h1>User does not exist!</h1>';
+  die();
+}
+
+
+$editform = new tutor_registration_edit_form(NULL, array('customdata' => array('userid' => $userid)));
+if ($editform->is_cancelled()) {
+  redirect(new moodle_url('http://peoples-uni.org'));
+}
+elseif ($data = $editform->get_data()) {
+
+  $application = new object();
+
+  $dataitem = $data->reasons;
+  if (empty($dataitem)) $dataitem = '';
+  $application->reasons = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $dataitem = $data->education;
+  if (empty($dataitem)) $dataitem = '';
+  $application->education = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $dataitem = $data->tutoringexperience;
+  if (empty($dataitem)) $dataitem = '';
+  $application->tutoringexperience = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $dataitem = $data->currentjob;
+  if (empty($dataitem)) $dataitem = '';
+  $application->currentjob = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $dataitem = $data->currentrole;
+  if (empty($dataitem)) $dataitem = '';
+  $application->currentrole = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $dataitem = $data->otherinformation;
+  if (empty($dataitem)) $dataitem = '';
+  $application->otherinformation = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $dataitem = $data->howfoundpeoples;
+  if (empty($dataitem)) $dataitem = '0';
+  $dataitem = strip_tags($dataitem);
+  $application->howfoundpeoples = $dataitem;
+
+  $dataitem = $data->howfoundorganisationname;
+  if (empty($dataitem)) $dataitem = '';
+  $application->howfoundorganisationname = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $DB->insert_record('peoples_tutor_registration', $application);(**)update
+
+  redirect(new moodle_url($CFG->wwwroot . '/course/tutor_registration_form_success.php'));(**)
+}
+
+
+// Print the form
+
+$PAGE->set_title("People's Open Access Education Initiative Tutor Registration Edit Form");
+$PAGE->set_heading('Peoples-uni Tutor Registration Edit Form');
+
+echo $OUTPUT->header();
+
+$editform->display();
+
+echo $OUTPUT->footer();
+
+
+function sendapprovedmail($email, $subject, $message) {
+  global $CFG;
+
+  // Dummy User
+  $user = new stdClass();
+  $user->id = 999999999;
+  $user->email = $email;
+  $user->maildisplay = true;
+  $user->mnethostid = $CFG->mnet_localhost_id;
+
+  $supportuser = new stdClass();
+  $supportuser->email = 'apply@peoples-uni.org';
+  $supportuser->firstname = "People's Open Access Education Initiative: Peoples-uni";
+  $supportuser->lastname = '';
+  $supportuser->maildisplay = true;
+
+  //$user->email = 'alanabarrett0@gmail.com';
+  $ret = email_to_user($user, $supportuser, $subject, $message);
+
+  //$user->email = 'applicationresponses@peoples-uni.org';
+  //$user->email = 'alanabarrett0@gmail.com';
+  //email_to_user($user, $supportuser, $email . ' Sent: ' . $subject, $message);
+
+  return $ret;
+}
---
+++ course/tutor_registration_edit_form.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Tutor Registration Edit form for Peoples-uni Tutors (Form Class)
+ */
+
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->libdir.'/formslib.php');
+require_once($CFG->libdir.'/completionlib.php');
+
+class tutor_registration_edit_form extends moodleform {
+
+  function definition() {
+    global $DB, $CFG;
+
+    $mform = $this->_form;
+    $customdata = $this->_customdata['customdata'];
+    $userid = $customdata['userid'];(**)use to load default data or instead pass tutor application id???
+
+
+    $mform->addElement('header', 'top', 'Instructions');
+
+    $mform->addElement('static', 'instuctions', '',
+'
+<p>Edit Volunteer Registration.<br /></p>
+');
+
+
+    $mform->addElement('header', 'personaldetails', 'Personal details');
+
+    $mform->addElement('textarea', 'reasons', 'Reasons for wanting to volunteer for Peoples-uni', 'wrap="HARD" rows="10" cols="100"');
+    $mform->addRule('reasons', 'Reasons for wanting to volunteer for Peoples-uni is required', 'required', null, 'client');
+    $mform->addElement('static', 'explainreasons', '&nbsp;', 'Please tell us your reasons for wanting to volunteer for Peoples-uni in up to 150 words.<br />');
+
+
+    $mform->addElement('header', 'educationdetails', 'Education and Employment details');
+
+    $mform->addElement('textarea', 'education', 'Relevant qualifications (academic and professional)', 'wrap="HARD" rows="10" cols="100"');
+    $mform->addRule('education', 'Relevant qualifications (academic and professional) is required', 'required', null, 'client');
+    $mform->addElement('static', 'explaineducation', '&nbsp;', 'Add details about any of your Relevant qualifications (academic and professional).<br />
+If you have a degree please indicate name of degree, awarding institution and also the language of instruction.<br />
+If you have a postgraduate qualification, please indicate name of qualification, awarding institution and also the language of instruction.<br />');
+
+    $mform->addElement('textarea', 'tutoringexperience', 'Educational/tutoring experience', 'wrap="HARD" rows="10" cols="100"');
+    $mform->addElement('static', 'explaintutoringexperience', '&nbsp;', 'Please briefly tell us about any educational/tutoring experience you may have.<br />');
+
+    $mform->addElement('textarea', 'currentjob', 'Current employer', 'wrap="HARD" rows="10" cols="100"');
+    $mform->addRule('currentjob', 'Current employer is required', 'required', null, 'client');
+    $mform->addElement('static', 'explaincurrentjob', '&nbsp;', 'Add name of your current employer.<br />');
+
+    $mform->addElement('textarea', 'currentrole', 'Current role', 'wrap="HARD" rows="10" cols="100"');
+    $mform->addRule('currentrole', 'Current role is required', 'required', null, 'client');
+    $mform->addElement('static', 'explaincurrentrole', '&nbsp;', 'Add details about your current role.<br />');
+
+    $mform->addElement('textarea', 'otherinformation', 'Other information', 'wrap="HARD" rows="10" cols="100"');
+    $mform->addElement('static', 'explainotherinformation', '&nbsp;', 'Any other information you want us to know about you.<br />');
+
+
+    $mform->addElement('header', 'howfounddetails', 'How did you hear about Peoples-uni?');
+
+    $howfoundpeoplesname[  ''] = 'Select...';
+    $howfoundpeoplesname['10'] = 'Informed by another Peoples-uni student or tutor';
+    $howfoundpeoplesname['20'] = 'Informed by someone else';
+    $howfoundpeoplesname['30'] = 'Facebook';
+    $howfoundpeoplesname['40'] = 'Internet advertisement';
+    $howfoundpeoplesname['50'] = 'Link from another website or discussion forum';
+    $howfoundpeoplesname['60'] = 'I used a search engine to look for courses, volunteering opportunities or other';
+    $howfoundpeoplesname['70'] = 'Referral from Partnership Institution';
+    $howfoundpeoplesname['80'] = 'Read or heard about from news article, journal or advertisement';
+    $mform->addElement('select', 'howfoundpeoples', 'How did you hear about Peoples-uni?', $howfoundpeoplesname);
+    $mform->addRule('howfoundpeoples', 'How did you hear about Peoples-uni is required', 'required', null, 'client');
+    $mform->addElement('static', 'explainhowfoundpeoples', '&nbsp;', 'Select the option that best describes how you heard about Peoples-uni.<br />');
+
+    $mform->addElement('text', 'howfoundorganisationname', 'Name of the organisation or person from whom you heard about Peoples-uni', 'maxlength="100" size="50"');
+    $mform->setType('howfoundorganisationname', PARAM_MULTILANG);
+    $mform->addElement('static', 'explainhowfoundorganisationname', '&nbsp;', 'Please enter the name of the organisation, person or website from whom you heard about Peoples-uni.<br />');
+
+
+    $this->add_action_buttons(false, 'Submit Form');
+
+    //$this->set_data($data);
+  }
+
+
+  function validation($data, $files) {
+    global $DB;
+
+    $errors = parent::validation($data, $files);
+
+    return $errors;
+  }
+}
