05134db40a0bbacf341d799582c7e3ebf807e0bc
Alan Barrett <alanabarrett0@gmail.com>
2014-02-14 22:23:40 +0000

    Take out deprecated magic quotes

--- course/fpdf/font/makefont/makefont.php
+++ course/fpdf/font/makefont/makefont.php
@@ -298,7 +298,7 @@ function CheckTTF($file)
 function MakeFont($fontfile,$afmfile,$enc='cp1252',$patch=array(),$type='TrueType')
 {
     //Generate a font definition file
-    set_magic_quotes_runtime(0);
+    //set_magic_quotes_runtime(0);
     ini_set('auto_detect_line_endings','1');
     if($enc)
     {
