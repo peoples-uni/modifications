93e1f23595fd6435a5f688ec81aaeb781d0bf75c
Alan Barrett <alanabarrett0@gmail.com>
2014-10-13 19:05:09 +0100

    Wording

--- course/registration_form_success.php
+++ course/registration_form_success.php
@@ -18,7 +18,7 @@ echo $OUTPUT->header();
 echo $OUTPUT->heading("People's Open Access Education Initiative Registration Form Successfully Submitted");
 
 echo '<p>';
-echo 'Thank you for registering. You should receive an email copy of your registration. If after an hour or two you have not received the email contact our support staff at - <strong><a href="mailto:apply@peoples-uni.org">apply@peoples-uni.org</a></strong> with your details.';
+echo 'Thank you for filling in this form. You should receive an email copy of your form with further information. If after an hour or two you have not received the email contact our support staff at - <strong><a href="mailto:apply@peoples-uni.org">apply@peoples-uni.org</a></strong> with your details.';
 echo '</p>';
 
 echo $OUTPUT->footer();
