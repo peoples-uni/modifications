d5cb61ce558d96cff8daba7266505748a7bf14a2
Alan Barrett <alanabarrett0@gmail.com>
2014-10-23 16:09:15 +0100

    Due date can be zero

--- course/track_quizzes.php
+++ course/track_quizzes.php
@@ -275,7 +275,8 @@ foreach ($track_submissions as $index => $track_submission) {
   elseif (!empty($track_submission->cutoff   ) && ($track_submission->submissiontime <= $track_submission->cutoff   )) $z = 'Within Cut-off';
   elseif (!empty($track_submission->extension)) $z = 'Outside Extension!!!';
   elseif (!empty($track_submission->cutoff   )) $z = 'Outside Cut-off!!!';
-  else                                          $z = 'Outside Due Date!!!';
+  elseif (!empty($track_submission->due      )) $z = 'Outside Due Date!!!';
+  else                                          $z = '';
   $rowdata[] = $z;
 
   $n++;
