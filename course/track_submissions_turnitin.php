b4cead51f885dce561f9264f2c4089408fb8d0bb
Alan Barrett <alanabarrett0@gmail.com>
2016-02-13 15:07:14 +0000

    Correct array

--- course/track_submissions_turnitin.php
+++ course/track_submissions_turnitin.php
@@ -282,7 +282,7 @@ foreach ($track_submissions as $index => $track_submission) {
   //if (substr_count($track_submission->submissionhistory, '(')  > 1) $rowdata[] = $track_submission->submissionhistory;
   //else $rowdata[] = '';
 
-  if (!empty($course_code_by_courseid[$track_submission->courseid]->course_code . '#' . $track_submission->userid])) {
+  if (!empty($number_of_submissions_by_course_by_user[$course_code_by_courseid[$track_submission->courseid]->course_code . '#' . $track_submission->userid])) {
     $number_of_submissions = $number_of_submissions_by_course_by_user[$course_code_by_courseid[$track_submission->courseid]->course_code . '#' . $track_submission->userid]->number_submissions;
     if ($number_of_submissions > 1) $number_of_submissions = " ($number_of_submissions)";
     else $number_of_submissions = '';
