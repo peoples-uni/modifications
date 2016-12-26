223787cfad7e4fddd6778b82ae31e691a4925b0c
Alan Barrett <alanabarrett0@gmail.com>
2016-12-26 12:57:31 +0000

    Do you intend to take the Final Assignment?

---
+++ course/xapplication_form_returning_student_form.php
@@ -0,0 +1,206 @@
+<?php
+
+/**
+ * Application form for Peoples-uni for Returning Students (Form Class)
+ */
+
+/*
+CREATE TABLE mdl_late_applications_allowed (
+  id BIGINT(10) UNSIGNED NOT NULL auto_increment,
+  userid BIGINT(10) unsigned NOT NULL DEFAULT 0,
+  approverid BIGINT(10) unsigned NOT NULL DEFAULT 0,
+  datesubmitted BIGINT(10) unsigned NOT NULL DEFAULT 0,
+  deadline BIGINT(10) unsigned NOT NULL DEFAULT 0,
+  CONSTRAINT PRIMARY KEY (id)
+);
+CREATE INDEX mdl_late_applications_allowed_ix ON mdl_late_applications_allowed (userid);
+*/
+
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->libdir.'/formslib.php');
+require_once($CFG->libdir.'/completionlib.php');
+
+class application_form_returning_student_form extends moodleform {
+
+  function definition() {
+    global $DB, $CFG, $USER;
+
+    $mform    = $this->_form;
+
+    $mform->addElement('header', 'top', 'Instructions');
+    $mform->setExpanded('top');
+
+    $mform->addElement('static', 'instuctions', '',
+'<p><strong>Please read the information in </strong><a href="http://www.peoples-uni.org/content/who-should-apply">Who should apply (Click Here)</a><strong> before submitting this form, particularly see the information about </strong><a href="http://peoples-uni.org/content/course-fees">Course fees (Click Here)</a></p>
+<p><strong>Use this form to apply to do course modules. You must have already been registered in Moodle. You need to enter the user name that you use when logging into Moodle in the form below.</strong></p>
+<p><strong>If you have not been registered in Moodle you must apply by </strong><a href="http://courses.peoples-uni.org/course/registration.php">Clicking Here</a><strong> first.</strong></p>
+<p>For inquires about course enrolment or payment please send an e-mail to <a href="mailto:apply@peoples-uni.org?subject=Registration or payment query">apply@peoples-uni.org</a></p>
+<p><strong>Note:</strong> You must complete the fields marked with a red <span style="color:#ff0000">*</span>.</p>
+<p><strong>Note:</strong> You must submit your application on or before ' . gmdate('jS F Y', get_config(NULL, 'peoples_last_application_date')) . '.</p>
+<p><strong>You should receive an e-mail with a copy of your application when you submit this form. If you do not, it means that we cannot reach your e-mail address. In that case please send an e-mail to <a href="mailto:apply@peoples-uni.org">apply@peoples-uni.org</a></strong></p>');
+
+
+    $semester_current = $DB->get_record('semester_current', array('id' => 1));
+    $mform->addElement('header', 'modules', "Course Module Selection for Semester $semester_current->semester");
+    $mform->setExpanded('modules');
+
+    // Ability to submit form (no matter what) is given by the "Manager" role which has moodle/site:viewparticipants
+    // (administrator also has moodle/site:viewparticipants)
+    $ismanager = has_capability('moodle/site:viewparticipants', context_system::instance());
+
+    // Allow specified Students who have been given a later than normal deadline to apply late
+    $late_application = FALSE;
+    if (!empty($USER->id)) {
+      $late_applications_allowed = $DB->get_record('late_applications_allowed', array('userid' => $USER->id));
+      if (!empty($late_applications_allowed)) {
+        $deadline = $late_applications_allowed->deadline;
+        if (time() < $deadline) {
+          $late_application = TRUE;
+        }
+      }
+    }
+
+    if (!($ismanager || $late_application)) {
+      $open_modules = $DB->get_records('activemodules', array('modulefull' => 0));
+      if (empty($open_modules)) {
+        redirect($CFG->wwwroot . '/course/closed.php');
+      }
+    }
+
+    $activemodules = $DB->get_records('activemodules', NULL, 'fullname ASC');
+
+    $listforselect = array();
+    $listforselect[''] = 'Select...';
+    $listforunavailable = array();
+    foreach ($activemodules as $activemodule) {
+      if (!$activemodule->modulefull || ($ismanager || $late_application)) {
+        $listforselect[$activemodule->course_id] = $activemodule->fullname;
+      }
+      else {
+        $listforunavailable[] = "'" . $activemodule->fullname . "'";
+      }
+    }
+    $count = count($listforunavailable);
+    $listforunavailable = implode(", ", $listforunavailable);
+
+    $_SESSION['peoples_filling_in_application_form'] = time();
+    //$text = "Please select the first course module you are applying for from the drop down box. Note: you should not apply for 'Masters dissertation' until given permission to do so. Note: Please do not apply for 'Scientific decision-making in health-care' if you are an MPH student. Note: you should only apply for 'Patient Safety in Practice' if you are doing the Certificate in Patient Safety and also if you have already completed the module 'Patient Safety' which is required for the Certificate in Patient Safety.";
+    //$text = "Please select the first course module you are applying for from the drop down box. Note: The 'Masters dissertation' is restricted to those who have passed sufficient prior modules. Note: Please do not apply for 'Scientific decision-making in health-care' if you are an MPH student. Note: you should only apply for 'Patient Safety in Practice' if you are doing the Certificate in Patient Safety and also if you have already completed the module 'Patient Safety' which is required for the Certificate in Patient Safety.";
+    $text = "Please select the first course module you are applying for from the drop down box.
+    <br /><strong>Note:</strong> The 'Masters dissertation' is restricted to those who have passed sufficient prior modules.
+    <br /><strong>Note:</strong> If you are applying for the 'Masters dissertation' module, you also need to separately submit a provisional topic for your dissertation. Please <strong><a href=\"" . $CFG->wwwroot . "/course/dissertation.php\" target=\"_blank\">Click Here on the Dissertation Topic Form</a></strong> to do this. You will find some helpful information there.
+    <br />You MUST submit this Course Application Form, but your application will not be approved if you have not also submitted a provisional topic.
+    <br /><strong>Note:</strong> The 'Global Mental Health' module is new and not available to students enrolled on the MMU programme.";
+    //<br /><strong>Note:</strong> Please do not apply for 'Scientific decision-making in health-care' if you are an MPH student.";
+//<br /><strong>Note:</strong> You should only apply for 'Patient Safety in Practice' if you are doing the Certificate in Patient Safety and also if you have already completed the module 'Patient Safety' which is required for the Certificate in Patient Safety.
+    if ($count > 1) {
+      //$text .= ' Note: ' . $listforunavailable . ' are not available for this semester because they are full.';
+      $text .= '<br /><strong>Note:</strong> ' . $listforunavailable . ' are not available for this semester because they are full.';
+    }
+    elseif ($count == 1) {
+      //$text .= ' Note: ' . $listforunavailable . ' is not available for this semester because it is full.';
+      $text .= '<br /><strong>Note:</strong> ' . $listforunavailable . ' is not available for this semester because it is full.';
+    }
+
+    $mform->addElement('select', 'course_id_1', 'First module', $listforselect);
+    $mform->addRule('course_id_1', 'First Module is required', 'required', null, 'client');
+    $mform->addElement('static', 'explain1', '&nbsp;', $text . '<br />');
+
+    $mform->addElement('select', 'course_id_2', 'Second module', $listforselect);
+    $mform->addElement('static', 'explain2', '&nbsp;', 'If you want do apply to do two modules in the same semester, select the second course module here. Please realise that both modules will run at the same time and the workload may be heavy, be sure that you do have the time if you elect to take two modules in the same semester.<br /><br />');
+
+    $mform->addElement('select', 'course_id_alternate', 'Alternate module', $listforselect);
+    $mform->addElement('static', 'explain3', '&nbsp;', 'If a module you want to take is full then please indicate which module is your preferred alternate choice.<br /><br />');
+
+    $listforselect = array();
+    //$listforselect[1] = 'No, continue with Peoples-uni';
+    //$listforselect[3] = 'I am already enrolled in MMU MPH';
+    //$listforselect[4] = 'Yes, apply for Peoples-uni MPH';
+    //$listforselect[5] = 'I am already enrolled in Peoples-uni MPH';
+// Judith suggested...
+//    $listforselect[4] = 'Yes, apply for Peoples-uni MPH';
+//    $listforselect[1] = 'No, continue with Peoples-uni';
+// I changed to...
+    $listforselect[1] = 'No, continue with Peoples-uni';
+    $listforselect[4] = 'Yes, apply for Peoples-uni MPH';
+    $listforselect[5] = 'I am already enrolled in Peoples-uni MPH';
+    $listforselect[3] = 'I am already enrolled in MMU MPH';
+    //$listforselect[2] = 'Yes, apply for MMU MPH';
+    //$listforselect[6] = 'Yes, apply for OTHER MPH';
+    //$listforselect[7] = 'I am already enrolled in OTHER MPH';
+    $mform->addElement('select', 'applymmumph', 'Apply for Peoples-uni Master of Public Health programme', $listforselect);
+    //$mform->addElement('static', 'explainapplymmumph', '&nbsp;', 'Do you want to apply for enrolment in the Master of Public Health programme (please note the fees <a href="http://www.peoples-uni.org/book/course-fees" target="_blank">http://www.peoples-uni.org/book/course-fees</a>)?<br />
+//Please do not apply if this is your first semester.<br /><br />');
+    $mform->addElement('static', 'explainapplymmumph', '&nbsp;', 'If you have already passed two modules at Masters level (50%), and would like to study for a Master of Public Health (MPH) or Diploma with Peoples-uni, please indicate that you want to apply for the MPH programme. If you have previously applied for the Peoples-uni MPH programme, please do so again, to make sure we know your intentions. For more information on the criteria and rules applying to the MPH programme, please follow this link <a href="http://www.peoples-uni.org/node/232" target="_blank">http://www.peoples-uni.org/node/232</a><br /><br />');
+
+    $listforselect = array();
+    $listforselect[1] = 'Yes, I will submit the final assignment for the module(s)';
+    $listforselect[2] = 'I will complete the module(s) without taking the final assignment, but would like to earn a Certificate of Participation';
+    $listforselect[3] = 'I will study the module materials without actively participating in discussions';
+    $mform->addElement('select', 'take_final_assignment', 'Do you intend to take the Final Assignment?', $listforselect);
+    $mform->setDefault('take_final_assignment', 1);
+    $mform->addElement('static', 'explaintake_final_assignment', '&nbsp;', 'Choose an option that most closely describes your intentions.');
+
+//    $listforselect = array();
+//    $listforselect[1] = 'No';
+//    $listforselect[2] = 'Yes';
+//    $listforselect[3] = 'I am already enrolled in the Certificate in Patient Safety';
+//    $mform->addElement('select', 'applycertpatientsafety', 'Apply for Certificate in Patient Safety', $listforselect);
+//    $mform->addElement('static', 'explainapplycertpatientsafety', '&nbsp;', 'Do you want to apply for enrolment in the Certificate in Patient Safety (see <a href="http://www.peoples-uni.org/node/281" target="_blank">http://www.peoples-uni.org/node/281</a>)?<br />
+//For this certificate you will need to complete the Evidence Based Practice, Patient Safety & Patient Safety in Practice modules.<br />');
+
+    $mform->addElement('header', 'personaldetails', 'Your Existing Moodle User Name');
+    $mform->setExpanded('personaldetails');
+
+    $mform->addElement('text', 'username', 'Moodle Username', 'maxlength="100" size="50"');
+    $mform->addRule('username', 'Moodle Username is required', 'required', null, 'client');
+    $mform->setType('username', PARAM_MULTILANG);
+    $mform->addElement('static', 'explainusername', '&nbsp;', 'The user name you use to login to your course modules.<br />');
+
+
+    $mform->addElement('header', 'scholorshipdetails', 'Scholarship');
+    $mform->setExpanded('scholorshipdetails');
+
+    $mform->addElement('static', 'explainscholarship', '&nbsp;', 'If you cannot afford the fees, we may be able to assist in approved cases. If you would like to apply for a reduction or waiver of the <a href="http://peoples-uni.org/content/course-fees" target="_blank">fees (click here)</a>, please provide details on:<br />
+1. What is your current employment AND monthly gross income<br />
+2. What is the reason you are unable to pay the fees<br />
+3. Whether you are able to pay a portion of the fees and if so how much<br />
+4. How you plan to use the skills/qualifications you will gain from Peoples-uni for the health of the population (up to 150 words)<br />');
+    $mform->addElement('textarea', 'scholarship', '&nbsp;', 'wrap="HARD" rows="10" cols="100" style="width:auto"');
+
+
+    $mform->addElement('header', 'whynotcompletedetails', 'Previous Semester');
+    $mform->setExpanded('whynotcompletedetails');
+
+    $mform->addElement('static', 'explainwhynotcomplete', '&nbsp;', 'If you are a returning student and did not complete your previous semester, please explain why this was so.');
+    $mform->addElement('textarea', 'whynotcomplete', '&nbsp;', 'wrap="HARD" rows="10" cols="100" style="width:auto"');
+
+
+    $this->add_action_buttons(false, 'Submit Form');
+  }
+
+
+  function validation($data, $files) {
+    global $DB;
+
+    $errors = parent::validation($data, $files);
+
+    if ($data['course_id_1'] === $data['course_id_2']) $errors['course_id_1'] = "You have selected the same module as your first and second choice. Either remove the second selection (by selecting the 'Select...' message at the top of the option list) or change the second module selected.";
+
+    $user_record = $DB->get_record('user', array('username' => $data['username']));
+    if (empty($user_record)) {
+      $errors['username'] = 'The Peoples Uni Moodle Username you have entered does not match any Moodle Username.';
+    }
+    else {
+      $oldapplication = $DB->get_record('peoplesapplication', array('userid' => $user_record->id), '*', IGNORE_MULTIPLE);
+      if (empty($oldapplication)) {
+        $oldapplication = $DB->get_record('peoplesregistration', array('userid' => $user_record->id), '*', IGNORE_MULTIPLE);
+        if (empty($oldapplication)) $errors['username'] = 'The Peoples Uni Moodle Username you have entered does not match any Moodle Student Username.';
+      }
+    }
+
+    return $errors;
+  }
+}
---
+++ course/xapplication_form_student.php
@@ -0,0 +1,285 @@
+<?php
+
+/**
+ * Application form for Peoples-uni Students
+ */
+
+require_once('../config.php');
+require_once('xapplication_form_returning_student_form.php');
+
+$PAGE->set_context(context_system::instance());
+
+$PAGE->set_pagelayout('standard');
+$PAGE->set_url('/course/application_form_student.php');
+
+
+$editform = new application_form_returning_student_form(NULL, array('customdata' => array()));
+if ($editform->is_cancelled()) {
+  redirect(new moodle_url('http://peoples-uni.org'));
+}
+elseif ($data = $editform->get_data()) {
+  unset($_SESSION['peoples_filling_in_application_form']);
+
+  $application = new object();
+
+  // Should be a transaction! (or use $application->id, but id is less than sid and I do not want to mess with live system now)
+  $peoplessid = $DB->get_record('peoplessid', array('id' => 1));
+  $peoplessid->sid = $peoplessid->sid + 1;
+  //$DB->update_record('peoplessid', $peoplessid);
+  $application->sid = $peoplessid->sid;
+
+  $application->state = 0;
+
+  $dataitem = $data->course_id_1;
+  if ($dataitem === '' || (is_numeric($dataitem) && $dataitem == 0)) {
+    $application->course_id_1 = 0;
+    $application->coursename1 = '';
+  }
+  else {
+    $application->course_id_1 = $dataitem; // $application->course_id_1 is not used
+    $course = $DB->get_record('course', array('id' => $dataitem));
+    $application->coursename1 = $course->fullname;
+  }
+
+  $dataitem = $data->course_id_2;
+  if ($dataitem === '' || (is_numeric($dataitem) && $dataitem == 0)) {
+    $application->course_id_2 = 0;
+    $application->coursename2 = '';
+  }
+  else {
+    $application->course_id_2 = $dataitem; // $application->course_id_2 is not used
+    $course = $DB->get_record('course', array('id' => $dataitem));
+    $application->coursename2 = $course->fullname;
+  }
+
+  $dataitem = $data->course_id_alternate;
+  if ($dataitem === '' || (is_numeric($dataitem) && $dataitem == 0)) {
+    $application->course_id_alternate = 0;
+    $application->alternatecoursename = '';
+  }
+  else {
+    $course = $DB->get_record('course', array('id' => $dataitem));
+    $application->alternatecoursename = $course->fullname;
+  }
+
+  $dataitem = $data->applymmumph;
+  if (empty($dataitem)) $dataitem = 0;
+  $application->applymmumph = $dataitem;
+  $applymmumphtext = array(0 => '', 1 => 'No', 2 => 'Yes', 3 => 'Already');
+  $applymmumphtext[1] = 'No, continue with Peoples-uni';
+  $applymmumphtext[2] = 'Yes, apply for MMU MPH';
+  $applymmumphtext[3] = 'I am already enrolled in MMU MPH';
+  $applymmumphtext[4] = 'Yes, apply for Peoples-uni MPH';
+  $applymmumphtext[5] = 'I am already enrolled in Peoples-uni MPH';
+  $applymmumphtext[6] = 'Yes, apply for OTHER MPH';
+  $applymmumphtext[7] = 'I am already enrolled in OTHER MPH';
+  $applymmumphtext = $applymmumphtext[$application->applymmumph];
+
+  $dataitem = $data->take_final_assignment;
+  if (empty($dataitem)) $dataitem = 0;
+  $application->take_final_assignment = $dataitem;
+  $take_final_assignmenttext = array(0 => '', 1 => 'No', 2 => 'Yes', 3 => 'Already');
+  $take_final_assignmenttext[1] = 'Yes, I will submit the final assignment for the module(s)';
+  $take_final_assignmenttext[2] = 'I will complete the module(s) without taking the final assignment, but would like to earn a Certificate of Participation';
+  $take_final_assignmenttext[3] = 'I will study the module materials without actively participating in discussions';
+  $take_final_assignmenttext = $take_final_assignmenttext[$application->take_final_assignment];
+
+  $dataitem = $data->applycertpatientsafety;
+  if (empty($dataitem)) $dataitem = 0;
+  $application->applycertpatientsafety = $dataitem;
+  $applycertpatientsafetytext = array(0 => '', 1 => 'No', 2 => 'Yes', 3 => 'Already');
+  $applycertpatientsafetytext = $applycertpatientsafetytext[$application->applycertpatientsafety];
+
+  $dataitem = $data->scholarship;
+  if (empty($dataitem)) $dataitem = '';
+  $application->scholarship = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $dataitem = $data->whynotcomplete;
+  if (empty($dataitem)) $dataitem = '';
+  $application->whynotcomplete = htmlspecialchars($dataitem, ENT_COMPAT, 'UTF-8');
+
+  $user_record = $DB->get_record('user', array('username' => $data->username));
+
+  $application->username  = $user_record->username;
+  $application->userid    = $user_record->id;
+
+  $application->email     = $user_record->email;
+  $application->lastname  = $user_record->lastname;
+  $application->firstname = $user_record->firstname;
+  $application->city      = $user_record->city;
+  $application->country   = $user_record->country;
+
+  $oldapplication = $DB->get_record('peoplesapplication', array('userid' => $application->userid), '*', IGNORE_MULTIPLE);
+  if (empty($oldapplication)) {
+    $oldapplication = $DB->get_record('peoplesregistration', array('userid' => $application->userid), '*', IGNORE_MULTIPLE);
+  }
+
+  $application->reenrolment = 0;
+  // Set reenrolment only if the Student was actually at least partially enrolled in a previous (not current) semester
+  // Partially enrolled states:
+  // Octal Decimal
+  //    23      19
+  //    32      26
+  //    13      11
+  //    31      25
+  //    33      27
+  $semester_current = $DB->get_record('semester_current', array('id' => 1));
+  $previous_enrolments = $DB->get_records_sql('
+    SELECT id
+    FROM mdl_peoplesapplication
+    WHERE
+      userid=? AND
+      semester!=? AND
+      state IN (19, 26, 11, 25, 27)',
+    array($application->userid, $semester_current->semester));
+  if (!empty($previous_enrolments)) {
+    $application->reenrolment = 1;
+  }
+
+  $application->gender                = $oldapplication->gender;
+  $application->applicationaddress    = $oldapplication->applicationaddress;
+  $application->currentjob            = $oldapplication->currentjob;
+  $application->education             = $oldapplication->education;
+  $application->reasons               = $oldapplication->reasons;
+  $application->sponsoringorganisation= $oldapplication->sponsoringorganisation;
+
+  $application->dobyear               = $oldapplication->dobyear;
+  $application->dobmonth              = $oldapplication->dobmonth;
+  $application->dobday                = $oldapplication->dobday;
+  $application->dob                   = sprintf('%04d-%02d-%02d', $application->dobyear, $application->dobmonth, $application->dobday); // Actually $application->dob is not used
+
+  $application->employment            = $oldapplication->employment;
+  $application->qualification         = $oldapplication->qualification;
+  $application->higherqualification   = $oldapplication->higherqualification;
+
+  $application->nid                   = 80; // Returning Student Application
+
+  $application->datesubmitted         = time();
+
+  $semester_current = $DB->get_record('semester_current', array('id' => 1));
+  $application->semester = $semester_current->semester;
+
+  $application->currency = 'GBP'; // The DB default is no longer correct 20090526!
+  $application->methodofpayment = '';
+  $application->paymentidentification = '';
+
+  //$DB->insert_record('peoplesapplication', $application);
+
+
+  $body = get_config(NULL, 'peoples_top_application_ack_email');
+  $body = strip_tags($body);
+
+  $body = preg_replace('#(http://[^\s]+)[\s]+#', "$1\n\n", $body); // Make sure every URL is followed by 2 newlines, some mail readers seem to concatenate following stuff to the URL if this is not done
+                                                                   // Maybe they would behave better if Moodle/we used CRLF (but we currently do not)
+  $message = "$body\n\n";
+
+  if ($application->reenrolment == 0) {
+    $message .= "First Application for...\n\n";
+    $returning_in_title = 'First';
+  }
+  else {
+    $message .= "Returning Student Application for...\n\n";
+    $returning_in_title = 'Returning';
+  }
+
+  $message .= "Family Name: $application->lastname\n\n";
+  $message .= "First Name: $application->firstname\n\n";
+  $message .= "e-mail: $application->email\n\n";
+  $message .= "Submission ID (SID): $application->sid\n\n";
+  $message .= "Date Submitted: " . gmdate('d/m/Y H:i', $application->datesubmitted) . "\n\n";
+  $message .= "Semester: $application->semester\n\n";
+  $message .= "Module 1: $application->coursename1\n\n";
+  $message .= "Module 2: $application->coursename2\n\n";
+  $message .= "Alternate module: $application->alternatecoursename\n\n";
+  $message .= "Apply for MPH: $applymmumphtext\n\n";
+  $message .= "Take the Final Assignment: $take_final_assignmenttext\n\n";
+  $message .= "Apply for Certificate in Patient Safety: $applycertpatientsafetytext\n\n";
+  $message .= "City: $application->city\n\n";
+  $countryname = get_string_manager()->get_list_of_countries(false);
+  $message .= "Country: " . $countryname[$application->country] . "\n\n";
+  $message .= "Username: $application->username\n\n";
+  $message .= "Data from original application...\n\n";
+  $message .= "Date of Birth: $application->dob\n\n";
+  $message .= "Gender: $application->gender\n\n";
+  $message .= "Application Address:\n" . htmlspecialchars_decode($application->applicationaddress, ENT_COMPAT) . "\n\n";
+  $message .= "Reasons for wanting to enrol:\n" . htmlspecialchars_decode($application->reasons, ENT_COMPAT) . "\n\n";
+  $message .= "Sponsoring organisation:\n" . htmlspecialchars_decode($application->sponsoringorganisation, ENT_COMPAT) . "\n\n";
+
+    $employmentname[  ''] = 'Select...';
+    $employmentname[ '1'] = 'None';
+    $employmentname['10'] = 'Student';
+    $employmentname['20'] = 'Non-health';
+    $employmentname['30'] = 'Clinical (not specifically public health)';
+    $employmentname['40'] = 'Public health';
+    $employmentname['50'] = 'Other health related';
+    $employmentname['60'] = 'Academic occupation (e.g. lecturer)';
+  $message .= "Current Employment: " . $employmentname[$application->employment] . "\n\n";
+    $qualificationname[  ''] = 'Select...';
+    $qualificationname[ '1'] = 'None';
+    $qualificationname['10'] = 'Degree (not health related)';
+    $qualificationname['20'] = 'Health qualification (non-degree)';
+    $qualificationname['30'] = 'Health qualification (degree, but not medical doctor)';
+    $qualificationname['40'] = 'Medical degree';
+  $message .= "Higher Education Qualification: " . $qualificationname[$application->qualification] . "\n\n";
+    $higherqualificationname[  ''] = 'Select...';
+    $higherqualificationname[ '1'] = 'None';
+    $higherqualificationname['10'] = 'Certificate';
+    $higherqualificationname['20'] = 'Diploma';
+    $higherqualificationname['30'] = 'Masters';
+    $higherqualificationname['40'] = 'Ph.D.';
+    $higherqualificationname['50'] = 'Other';
+  $message .= "Postgraduate Qualification: " . $higherqualificationname[$application->higherqualification] . "\n\n";
+
+  $message .= "Current Employment Details:\n" . htmlspecialchars_decode($application->currentjob, ENT_COMPAT) . "\n\n";
+  $message .= "Other relevant qualifications or educational experience:\n" . htmlspecialchars_decode($application->education, ENT_COMPAT) . "\n\n";
+  $message .= "Scholarship:\n" . htmlspecialchars_decode($application->scholarship, ENT_COMPAT) . "\n\n";
+  $message .= "Why Not Completed Previous Semester:\n" . htmlspecialchars_decode($application->whynotcomplete, ENT_COMPAT) . "\n";
+
+  sendapprovedmail($application->email, "Peoples-uni $returning_in_title Application Form Submission From: $application->lastname, $application->firstname", $message);
+  //sendapprovedmail('apply@peoples-uni.org', "Peoples-uni $returning_in_title Application Form Submission From: $application->lastname, $application->firstname", $message);
+
+  redirect(new moodle_url($CFG->wwwroot . '/course/application_form_success.php'));
+}
+
+
+// Print the form
+
+$PAGE->set_title("People's Open Access Education Initiative Application Form");
+$PAGE->set_heading('Peoples-uni Course Application Form');
+
+echo $OUTPUT->header();
+
+$editform->display();
+
+echo $OUTPUT->footer();
+
+
+function sendapprovedmail($email, $subject, $message) {
+  global $CFG;
+
+  // Dummy User
+  $user = new stdClass();
+  $user->id = 999999999;
+  $user->email = $email;
+  $user->maildisplay = true;
+  $user->mnethostid = $CFG->mnet_localhost_id;
+
+  $supportuser = new stdClass();
+  $supportuser->email = 'apply@peoples-uni.org';
+  $supportuser->firstname = "People's Open Access Education Initiative: Peoples-uni";
+  $supportuser->lastname = '';
+  $supportuser->firstnamephonetic = NULL;
+  $supportuser->lastnamephonetic = NULL;
+  $supportuser->middlename = NULL;
+  $supportuser->alternatename = NULL;
+  $supportuser->maildisplay = true;
+
+  $user->email = 'alanabarrett0@gmail.com';
+  $ret = email_to_user($user, $supportuser, $subject, $message);
+
+  //$user->email = 'applicationresponses@peoples-uni.org';
+  //$user->email = 'alanabarrett0@gmail.com';
+  //email_to_user($user, $supportuser, $email . ' Sent: ' . $subject, $message);
+
+  return $ret;
+}
