d3e9c39899c3df8c9b0c96d08e16be59451884d0
Alan Barrett <alanabarrett0@gmail.com>
2017-05-08 15:26:05 +0100

    debug

--- course/posts.php
+++ course/posts.php
@@ -1235,9 +1235,9 @@ error_log("all_users... $name, usercount[name]: " . $usercount[$name]);
           $list_of_courses[$chosenmodule] = $chosenmodule;
         }
         foreach ($list_of_courses as $course_item) {
-error_log(strtolower(trim($all_user->lastname) . ', ' . trim($all_user->firstname) . 'XXX8167YYYY' . trim($course_item)));
-error_log("number_of_topics_with_substantial_posts_per_user_course[" . strtolower(trim($all_user->lastname) . ', ' . trim($all_user->firstname) . 'XXX8167YYYY' . trim($course_item)) . "]: " . $number_of_topics_with_substantial_posts_per_user_course[strtolower(trim($all_user->lastname) . ', ' . trim($all_user->firstname) . 'XXX8167YYYY' . trim($course_item))]);
+error_log("number_of_topics_with_substantial_posts_per_user_course[" . strtolower(trim($all_user->lastname) . ', ' . trim($all_user->firstname) . 'XXX8167YYYY' . trim($course_item)) . "]");
           if (empty($number_of_topics_with_substantial_posts_per_user_course[strtolower(trim($all_user->lastname) . ', ' . trim($all_user->firstname) . 'XXX8167YYYY' . trim($course_item))])) {
+error_log("Setting to No posts!");
             $missing_item = new stdClass();
             $missing_item->lastname = $all_user->lastname;
             $missing_item->firstname = $all_user->firstname;
