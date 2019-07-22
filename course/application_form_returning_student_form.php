5a85ab9de134b24b13372de21038d7675f6a9909
Alan Barrett <alanabarrett0@gmail.com>
2019-07-22 10:29:20 +0100

    New required icon

--- course/application_form_returning_student_form.php
+++ course/application_form_returning_student_form.php
@@ -39,7 +39,7 @@ particularly see the information about </strong><a href="https://peoples-uni.org
 If you have not been registered in Moodle you must apply by </strong><a href="https://courses.peoples-uni.org/course/registration.php">Clicking Here</a><strong> first.</strong></p>
 <p><strong>You need to enter the user name that you use when logging into Moodle in the form below.</strong></p>
 <p>For inquires about course enrolment or payment please send an e-mail to <a href="mailto:apply@peoples-uni.org?subject=Registration or payment query">apply@peoples-uni.org</a></p>
-<p><strong>Note:</strong> You must complete the fields marked with a red <span style="color:#ff0000">*</span>.</p>
+<p><strong>Note:</strong> You must complete the fields marked with a red <abbr class="initialism text-danger" title="Required"><i class="icon fa fa-exclamation-circle text-danger fa-fw "  title="Required" aria-label="Required"></i></abbr>.</p>
 <p><strong>Note:</strong> You must submit your application on or before ' . gmdate('jS F Y', get_config(NULL, 'peoples_last_application_date')) . '.</p>
 <p><strong>You should receive an e-mail with a copy of your application when you submit this form. If you do not, it means that we cannot reach your e-mail address. In that case please send an e-mail to <a href="mailto:apply@peoples-uni.org">apply@peoples-uni.org</a></strong></p>');
 
