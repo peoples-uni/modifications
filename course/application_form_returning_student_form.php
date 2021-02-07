045381b28dc574503bb5419cb148b0ecc95b2c24
Alan Barrett <alanabarrett0@gmail.com>
2021-02-07 12:20:18 +0000

    Wording

--- course/application_form_returning_student_form.php
+++ course/application_form_returning_student_form.php
@@ -159,26 +159,33 @@ If you have not been registered in Moodle you must apply by </strong><a href="ht
 
   if (!$already_mph) {
     $listforselect = array();
-    //$listforselect[1] = 'No, continue with Peoples-uni';
-    //$listforselect[2] = 'Yes, apply for MMU MPH';
-    //$listforselect[3] = 'I am already enrolled in MMU MPH';
-    //$listforselect[4] = 'Yes, apply for Peoples-uni MPH';
-    //$listforselect[5] = 'I am already enrolled in Peoples-uni MPH';
+      //$listforselect[1] = 'No, continue with Peoples-uni';
+      //$listforselect[2] = 'Yes, apply for MMU MPH';
+      //$listforselect[3] = 'I am already enrolled in MMU MPH';
+      //$listforselect[4] = 'Yes, apply for Peoples-uni MPH';
+      //$listforselect[5] = 'I am already enrolled in Peoples-uni MPH';
     $listforselect[1] = 'I don\'t intend to complete a full Masters programme';
     $listforselect[8] = 'I intend to enrol on one of the Masters programmes in future';
+    $listforselect[11] = 'I am part of the BHIS Cameroon cohort and plan to enrol in the MPH when eligible'
     $listforselect[4] = 'Yes, apply for Peoples-uni Masters-level programme';
-    //$listforselect[5] = 'I am already enrolled in Peoples-uni Masters-level programme';
+      //$listforselect[5] = 'I am already enrolled in Peoples-uni Masters-level programme';
     $listforselect[6] = 'Yes, apply for EUCLID MPH programme';
-    //$listforselect[7] = 'I am already enrolled in EUCLID MPH programme';
-    //$listforselect[6] = 'Yes, apply for OTHER MPH';
-    //$listforselect[7] = 'I am already enrolled in OTHER MPH';
-    $listforselect[10] = 'Yes, I am already enrolled in FPD Pretoria MPH programme';
-    $mform->addElement('select', 'applymmumph', 'Apply for Masters-level programme with Peoples-uni, EUCLID or FPD Pretoria?', $listforselect);
+      //$listforselect[7] = 'I am already enrolled in EUCLID MPH programme';
+      //$listforselect[6] = 'Yes, apply for OTHER MPH';
+      //$listforselect[7] = 'I am already enrolled in OTHER MPH';
+      //$listforselect[10] = 'Yes, I am already enrolled in FPD Pretoria MPH programme';
+    $listforselect[12] = 'Yes, apply for the BHIS Cameroon MPH programme';
+    $mform->addElement('select', 'applymmumph', 'Apply for Masters-level programme with Peoples-uni, EUCLID or BHIS Cameroon?', $listforselect);
     $mform->addElement('static', 'explainapplymmumph', '&nbsp;', 'If you have already passed two modules at Masters level (50%) and not failed any after resubmissions
-and wish to apply for the Masters-level programme with Peoples-uni or EUCLID, please indicate your preferences here. Note that the curriculum is the same. If you have previously applied for the Masters-level programme, please do so again, to make sure we know your intentions.<br />
-<strong>NOTE</strong>: If you already enrolled in the FPD Pretoria MPH programme, the two module requirement does not apply and you should indicate that you are already enrolled.<br />
+and wish to apply for the Masters-level programme with Peoples-uni, EUCLID or BHIS Cameroon, please indicate your preferences here. Note that the curriculum is the same. If you have previously applied for the Masters-level programme, please do so again, to make sure we know your intentions.<br />
 <strong>NOTE</strong>: There will be fees for the EUCLID MPH award, but note that these will only be payable after achieving the Peoples-uni award.<br />
 For more information on the criteria and rules applying to the Masters-level programme, please follow this link <a href="https://peoples-uni.org/content/peoples-uni-public-health-masters-level-award-programme-curriculum" target="_blank">https://peoples-uni.org/content/peoples-uni-public-health-masters-level-award-programme-curriculum</a><br /><br />');
+      //$mform->addElement('select', 'applymmumph', 'Apply for Masters-level programme with Peoples-uni, EUCLID or FPD Pretoria?', $listforselect);
+      //    $mform->addElement('static', 'explainapplymmumph', '&nbsp;', 'If you have already passed two modules at Masters level (50%) and not failed any after resubmissions
+      //and wish to apply for the Masters-level programme with Peoples-uni or EUCLID, please indicate your preferences here. Note that the curriculum is the same. If you have previously applied for the Masters-level programme, please do so again, to make sure we know your intentions.<br />
+      //<strong>NOTE</strong>: If you already enrolled in the FPD Pretoria MPH programme, the two module requirement does not apply and you should indicate that you are already enrolled.<br />
+      //<strong>NOTE</strong>: There will be fees for the EUCLID MPH award, but note that these will only be payable after achieving the Peoples-uni award.<br />
+      //For more information on the criteria and rules applying to the Masters-level programme, please follow this link <a href="https://peoples-uni.org/content/peoples-uni-public-health-masters-level-award-programme-curriculum" target="_blank">https://peoples-uni.org/content/peoples-uni-public-health-masters-level-award-programme-curriculum</a><br /><br />');
   }
 
     //20170717 removed: $mform->addElement('select', 'applymmumph', 'Apply for Peoples-uni Master of Public Health programme', $listforselect);
@@ -217,13 +224,13 @@ For more information on the criteria and rules applying to the Masters-level pro
     $mform->addElement('static', 'explainscholarship', '&nbsp;', 'If you cannot afford the Peoples-uni fees, we may be able to assist in approved cases.<br />
 Please note that there will be a separate process for applying for the EUCLID MPH including any applications for bursaries, after graduation from the Peoples-uni programme. We will write to you at that time, and the meantime we encourage you to think about saving money for this if you want to take up this option.<br />
 Whether you want the additional EUCLID award or not, you can still study with Peoples-uni and If you cannot afford the more modest Peoples-uni fees, then we may be able to assist in approved cases.<br />
-Payments and bursaries for those enrolled in the FPD Pretoria MPH are dealt with through FPD.<br />
 If you would like to apply for a reduction or waiver of the <a href="https://peoples-uni.org/content/course-fees-payment-options" target="_blank">fees (click here)</a>, please provide details on:<br />
 1. What is your current employment AND monthly gross income<br />
 2. What is the reason you are unable to pay the fees<br />
 3. Whether you are able to pay a portion of the fees and if so how much<br />
 4. How you plan to use the skills/qualifications you will gain from Peoples-uni for the health of the population (up to 150 words)<br />
 5. Please note that late applications for scholarships cannot be considered after ' . gmdate('jS F Y', get_config(NULL, 'peoples_last_application_date')));
+      //Payments and bursaries for those enrolled in the FPD Pretoria MPH are dealt with through FPD.<br />
     $mform->addElement('textarea', 'scholarship', '&nbsp;', 'wrap="HARD" rows="10" cols="100" style="width:auto"');
   }
 
